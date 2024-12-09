d1de099bf3f89f72a1052c4b662f3b4c73e83938
DedSerg <168771309+DedSerg@users.noreply.github.com>
2024-12-09 13:29:23 +0300

    Update settings.py

--- GameShop/settings.py
+++ GameShop/settings.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 Django settings for GameShop project.
 
@@ -128,4 +127,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-=======
+
